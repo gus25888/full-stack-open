6c6f80a67b35b0c8b6d6136b7c569952391b9cec
Gustavo Baeza Avello <gustavobaezaavello@gmail.com>
2024-11-13 17:59:58 -0300

    Exercises 1.6 to 1.11 done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,63 +1,75 @@
-const Part = ({ part, exercise }) => {
+import { useState } from 'react'
+
+const Title = ({ titleName }) => (<>
+  <h2>{titleName}</h2>
+</>)
+
+const Button = ({ handleClick, buttonText }) => (
+  <button onClick={handleClick}>{buttonText}</button>
+)
+
+const StatisticLine = ({ statisticName, statisticValue }) => (
+  <tr><td>{statisticName}</td><td>{statisticValue}</td></tr>
+)
+
+const Statistics = ({ indicators }) => {
+
+  if (indicators.all === 0) {
+    return (
+      <div>no feedback given</div>
+    )
+  }
+
   return (
-    <>
-      <p>
-        {part} {exercise}
-      </p>
-    </>)
-}
-
-
-const Header = ({ course }) => {
-  return (<>
-    <h1>{course}</h1>
-  </>)
-}
-
-const Content = ({ parts }) => {
-
-  return (
-    <>
-      <Part part={parts[0].name} exercise={parts[0].exercises} />
-      <Part part={parts[1].name} exercise={parts[1].exercises} />
-      <Part part={parts[2].name} exercise={parts[2].exercises} />
-    </>
+    <table>
+      <tbody>
+        <StatisticLine statisticName='good' statisticValue={indicators.good} />
+        <StatisticLine statisticName='neutral' statisticValue={indicators.neutral} />
+        <StatisticLine statisticName='bad' statisticValue={indicators.bad} />
+        <StatisticLine statisticName='all' statisticValue={indicators.all} />
+        <StatisticLine statisticName='average' statisticValue={indicators.average} />
+        <StatisticLine statisticName='positive' statisticValue={indicators.positive + '%'} />
+      </tbody>
+    </table>
   )
 }
 
-const Total = ({ parts }) => {
-  const total = parts[0].exercises + parts[1].exercises + parts[2].exercises;
-  return (<>
-    <p>Number of exercises {total}</p>
-  </>)
-}
-
-
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  };
+  // guarda los clics de cada botón en su propio estado
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const getPunctuationTotal = () => {
+    return good + neutral + bad
+  }
+  const getPunctuationAverage = () => {
+    const total = getPunctuationTotal();
+    return (total === 0) ? 0 : ((good * 1) + (neutral * 0) + (bad * -1)) / total;
+  }
+  const getPunctuationPositive = () => {
+    const total = getPunctuationTotal();
+    return (total === 0) ? 0 : good / total * 100;
+  }
+
+  const indicators = {
+    'good': good,
+    'neutral': neutral,
+    'bad': bad,
+    'all': getPunctuationTotal(),
+    'average': getPunctuationAverage(),
+    'positive': getPunctuationPositive()
+  }
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div >
+      <Title titleName='give feedback' />
+      <Button handleClick={() => setGood(good + 1)} buttonText='good' />
+      <Button handleClick={() => setNeutral(neutral + 1)} buttonText='neutral' />
+      <Button handleClick={() => setBad(bad + 1)} buttonText='bad' />
+      <Title titleName='statistics' />
+      <Statistics indicators={indicators} />
+    </div>
   )
 }
 
